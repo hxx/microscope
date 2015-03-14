7a0049ea08dfde3f98fc1560ee447f01fbeb138c
hxx <yellowstar520@gmail.com>
2015-03-14 09:32:43 +0800

    Add Errors for Post

---
+++ client/helpers/errors.js
@@ -0,0 +1,6 @@
+// Local Collection
+Errors = new Mongo.Collection(null);
+
+throwError = function (message) {
+  Errors.insert({message: message});
+}
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.created = function () {
+  Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({
+  errorMessage: function (field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass:function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,14 +20,18 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
+
     Meteor.call('postInsert', post, function(error, result) {
       // display errors and exit
       if (error)
-        return alert(error.reason);
+        return throwError(error.reason);
 
       // show result if the post exist
       if (result.postExists)
-        alert('This link has already been posted.');
+        throwError('This link has already been posted.');
 
       Router.go('postPage', {_id: result._id});
     });
